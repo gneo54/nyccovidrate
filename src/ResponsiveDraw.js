2505884e461d3537eaab88f08777a104c4a0f8e4
Gean Martinez <gneo54@gmail.com>
2020-10-18 13:46:01 -0400

    alpha release

---
+++ src/ResponsiveDraw.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import Divider from "@material-ui/core/Divider";
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+//import VisibleItemList from '../containers/VisibleItemList'
+import Home from './Home';
+import AboutContent from './components/AboutContent';
+const drawerWidth = 240;
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  closeMenuButton: {
+    marginRight: 'auto',
+    marginLeft: 0,
+  },
+}));
+function ResponsiveDraw() {
+  const dummyCategories = ['Home', 'About']
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen)
+  }
+const drawer = (
+    <div>
+      <Divider />
+      <List>
+      <ListItem button key={'Home'} component={Link} to={"/" + ""}>
+            <ListItemText primary={'Home'} />
+          </ListItem>
+
+          <ListItem button key={'About'} component={Link} to={"/" + "About"}>
+            <ListItemText primary={'About'} />
+          </ListItem>
+
+        
+      </List>
+    </div>
+  );
+return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            NYC Covid Rate
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <BrowserRouter>
+      <nav className={classes.drawer}>
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <IconButton onClick={handleDrawerToggle} className={classes.closeMenuButton}>
+              <CloseIcon/>
+            </IconButton>
+            {drawer}
+          </Drawer>
+        </Hidden>
+<Hidden xsDown implementation="css">
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.toolbar} />
+            {drawer}
+          </Drawer>  
+        </Hidden>
+      </nav>
+      
+      <div className={classes.content}>
+        <div className={classes.toolbar} />    
+        <Switch>
+            <Route exact path="/"  component={Home}  />
+            <Route path="/About"   component={AboutContent} />
+            
+        </Switch>
+      </div>
+      </BrowserRouter>
+    </div>
+    
+  );
+}
+ResponsiveDraw.propTypes = {
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  container: PropTypes.object,
+};
+export default ResponsiveDraw;
